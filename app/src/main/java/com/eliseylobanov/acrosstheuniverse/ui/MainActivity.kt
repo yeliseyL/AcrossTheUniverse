1a82552a0d489b8822d463189a0accac4d2752fd
Elisey Lobanov <crimsonorama@gmail.com>
2021-03-30 18:56:06 +0300

    added collapsable toolbar

--- app/src/main/java/com/eliseylobanov/acrosstheuniverse/ui/MainActivity.kt
+++ app/src/main/java/com/eliseylobanov/acrosstheuniverse/ui/MainActivity.kt
@@ -2,11 +2,15 @@ package com.eliseylobanov.acrosstheuniverse.ui
 
 import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.eliseylobanov.acrosstheuniverse.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 const val DEFAULT_THEME = R.style.Theme_AcrossTheUniverse
 
@@ -16,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setNewTheme()
         setContentView(R.layout.activity_main)
         setUpNavigation()
+        setSupportActionBar(toolbar)
     }
 
     private fun setNewTheme() {
@@ -28,5 +33,16 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
         val navController = findNavController(R.id.nav_host_fragment)
         bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener{ _, destination, _ ->
+            title = when (destination.id) {
+                R.id.earthFragment -> "Across The Universe - Earth"
+                R.id.marsFragment -> "Across The Universe - Mars"
+                R.id.settingsFragment -> "Across The Universe - Settings"
+                R.id.weatherFragment -> "Across The Universe - Weather"
+                R.id.pictureOfDayFragment -> "Across The Universe - Pictures"
+                else -> "Across The Universe"
+            }
+        }
     }
 }
