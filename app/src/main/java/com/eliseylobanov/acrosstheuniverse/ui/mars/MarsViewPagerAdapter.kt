ad3839d5f675404f7a2a9904f7094ae9e02dd38d
Elisey Lobanov <crimsonorama@gmail.com>
2021-03-31 16:27:52 +0300

    some refactoring

--- app/src/main/java/com/eliseylobanov/acrosstheuniverse/ui/mars/MarsViewPagerAdapter.kt
+++ app/src/main/java/com/eliseylobanov/acrosstheuniverse/ui/mars/MarsViewPagerAdapter.kt
@@ -5,20 +5,46 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.eliseylobanov.acrosstheuniverse.R
 import com.eliseylobanov.acrosstheuniverse.databinding.MarsPhotoViewpagerItemBinding
+import com.eliseylobanov.acrosstheuniverse.entities.Earth
 import com.eliseylobanov.acrosstheuniverse.entities.Photo
+import com.eliseylobanov.acrosstheuniverse.ui.DataBindingAdapter
 
-class MarsViewPagerAdapter: ListAdapter<Photo, MarsViewPagerAdapter.PhotoViewHolder>(DiffCallback) {
+//class MarsViewPagerAdapter: ListAdapter<Photo, MarsViewPagerAdapter.PhotoViewHolder>(DiffCallback) {
+//
+//    class PhotoViewHolder(private var binding: MarsPhotoViewpagerItemBinding) :
+//        RecyclerView.ViewHolder(binding.root) {
+//        fun bind(photo: Photo) {
+//            binding.photo = photo
+//            binding.executePendingBindings()
+//        }
+//    }
+//
+//    companion object DiffCallback : DiffUtil.ItemCallback<Photo>() {
+//        override fun areItemsTheSame(oldItem: Photo, newItem: Photo): Boolean {
+//            return oldItem === newItem
+//        }
+//
+//        override fun areContentsTheSame(oldItem: Photo, newItem: Photo): Boolean {
+//            return oldItem.id == newItem.id
+//        }
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup,
+//                                    viewType: Int): PhotoViewHolder {
+//        return PhotoViewHolder(MarsPhotoViewpagerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+//    }
+//
+//    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+//        val photo = getItem(position)
+//        holder.bind(photo)
+//    }
+//}
 
-    class PhotoViewHolder(private var binding: MarsPhotoViewpagerItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(photo: Photo) {
-            binding.photo = photo
-            binding.executePendingBindings()
-        }
-    }
+class MarsViewPagerAdapter : DataBindingAdapter<Photo>(DiffCallback()) {
 
-    companion object DiffCallback : DiffUtil.ItemCallback<Photo>() {
+    class DiffCallback : DiffUtil.ItemCallback<Photo>() {
         override fun areItemsTheSame(oldItem: Photo, newItem: Photo): Boolean {
             return oldItem === newItem
         }
@@ -27,14 +53,5 @@ class MarsViewPagerAdapter: ListAdapter<Photo, MarsViewPagerAdapter.PhotoViewHol
             return oldItem.id == newItem.id
         }
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): PhotoViewHolder {
-        return PhotoViewHolder(MarsPhotoViewpagerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
-    }
-
-    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
-        val photo = getItem(position)
-        holder.bind(photo)
-    }
+    override fun getItemViewType(position: Int) = R.layout.mars_photo_viewpager_item
 }
--- app/src/main/java/com/eliseylobanov/acrosstheuniverse/ui/weather/WeatherViewPagerAdapter.kt
+++ app/src/main/java/com/eliseylobanov/acrosstheuniverse/ui/weather/WeatherViewPagerAdapter.kt
@@ -5,20 +5,46 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.eliseylobanov.acrosstheuniverse.R
 import com.eliseylobanov.acrosstheuniverse.databinding.WeatherViewpagerItemBinding
+import com.eliseylobanov.acrosstheuniverse.entities.Photo
 import com.eliseylobanov.acrosstheuniverse.entities.WeatherItem
+import com.eliseylobanov.acrosstheuniverse.ui.DataBindingAdapter
 
-class WeatherViewPagerAdapter : ListAdapter<WeatherItem, WeatherViewPagerAdapter.WeatherViewHolder>(DiffCallback) {
+//class WeatherViewPagerAdapter : ListAdapter<WeatherItem, WeatherViewPagerAdapter.WeatherViewHolder>(DiffCallback) {
+//
+//    class WeatherViewHolder(private var binding: WeatherViewpagerItemBinding) :
+//            RecyclerView.ViewHolder(binding.root) {
+//        fun bind(weather: WeatherItem) {
+//            binding.weather = weather
+//            binding.executePendingBindings()
+//        }
+//    }
+//
+//    companion object DiffCallback : DiffUtil.ItemCallback<WeatherItem>() {
+//        override fun areItemsTheSame(oldItem: WeatherItem, newItem: WeatherItem): Boolean {
+//            return oldItem === newItem
+//        }
+//
+//        override fun areContentsTheSame(oldItem: WeatherItem, newItem: WeatherItem): Boolean {
+//            return oldItem.messageID == newItem.messageID
+//        }
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup,
+//                                    viewType: Int): WeatherViewHolder {
+//        return WeatherViewHolder(WeatherViewpagerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+//    }
+//
+//    override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
+//        val weather = getItem(position)
+//        holder.bind(weather)
+//    }
+//}
 
-    class WeatherViewHolder(private var binding: WeatherViewpagerItemBinding) :
-            RecyclerView.ViewHolder(binding.root) {
-        fun bind(weather: WeatherItem) {
-            binding.weather = weather
-            binding.executePendingBindings()
-        }
-    }
+class WeatherViewPagerAdapter : DataBindingAdapter<WeatherItem>(DiffCallback()) {
 
-    companion object DiffCallback : DiffUtil.ItemCallback<WeatherItem>() {
+    class DiffCallback : DiffUtil.ItemCallback<WeatherItem>() {
         override fun areItemsTheSame(oldItem: WeatherItem, newItem: WeatherItem): Boolean {
             return oldItem === newItem
         }
@@ -27,14 +53,5 @@ class WeatherViewPagerAdapter : ListAdapter<WeatherItem, WeatherViewPagerAdapter
             return oldItem.messageID == newItem.messageID
         }
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): WeatherViewHolder {
-        return WeatherViewHolder(WeatherViewpagerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
-    }
-
-    override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
-        val weather = getItem(position)
-        holder.bind(weather)
-    }
+    override fun getItemViewType(position: Int) = R.layout.weather_viewpager_item
 }
